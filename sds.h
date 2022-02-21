9f826095a195df960219f821e583d6cf977870a4
Enno Rehling <enno.rehling@gmail.com>
2022-02-21 18:28:42 +0100

    fix gnuc syntax

--- sds.h
+++ sds.h
@@ -44,39 +44,39 @@ extern const char *SDS_NOINIT;
 typedef char *sds;
 
 #if defined(__GNUC__)
-#define PACK( __Declaration__ ) __Declaration__ __attribute__((__packed__))
+#define PACK( __Declaration__ ) struct __attribute__((__packed__)) __Declaration__
 #elif defined(_MSC_VER)
-#define PACK( __Declaration__ ) __pragma( pack(push, 1) ) __Declaration__ __pragma( pack(pop))
+#define PACK( __Declaration__ ) __pragma( pack(push, 1) ) struct __Declaration__ __pragma( pack(pop))
 #else
-#define PACK( __Declaration__ ) __Declaration__
+#define PACK( __Declaration__ ) struct __Declaration__
 #endif
 
 
 /* Note: sdshdr5 is never used, we just access the flags byte directly.
  * However is here to document the layout of type 5 SDS strings. */
-PACK(struct sdshdr5) {
+PACK(sdshdr5) {
     unsigned char flags; /* 3 lsb of type, and 5 msb of string length */
     char buf[];
 };
-PACK(struct sdshdr8) {
+PACK(sdshdr8) {
     uint8_t len; /* used */
     uint8_t alloc; /* excluding the header and null terminator */
     unsigned char flags; /* 3 lsb of type, 5 unused bits */
     char buf[];
 };
-PACK(struct sdshdr16) {
+PACK(sdshdr16) {
     uint16_t len; /* used */
     uint16_t alloc; /* excluding the header and null terminator */
     unsigned char flags; /* 3 lsb of type, 5 unused bits */
     char buf[];
 };
-PACK(struct sdshdr32) {
+PACK(sdshdr32) {
     uint32_t len; /* used */
     uint32_t alloc; /* excluding the header and null terminator */
     unsigned char flags; /* 3 lsb of type, 5 unused bits */
     char buf[];
 };
-PACK(struct sdshdr64) {
+PACK(sdshdr64) {
     uint64_t len; /* used */
     uint64_t alloc; /* excluding the header and null terminator */
     unsigned char flags; /* 3 lsb of type, 5 unused bits */
